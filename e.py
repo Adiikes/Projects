eaacdcb74a5611f7ea9909f066d161f92b38c382
Adiikes <adityanthesecond@gmail.com>
2022-05-04 16:43:54 -0400

    e.py
    Basically, something that tells you if the minute right now is odd or not.

---
+++ e.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from time import sleep
+
+odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
+
+right_this_minute = datetime.today().minute
+
+while True:
+    sleep(59.9)
+    if right_this_minute in odds:
+        print("This minute is odd.")
+    else:
+        print("This minute is not odd.")
+        
