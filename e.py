53add85373ae7017f3a2321b61d968941e7362fb
Adiikes <adityanthesecond@gmail.com>
2022-05-04 16:45:42 -0400

    Update e.py

--- e.py
+++ e.py
@@ -6,7 +6,7 @@ odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 3
 right_this_minute = datetime.today().minute
 
 while True:
-    sleep(59.9)
+    sleep(59.9) #Yes, I know this isn't 60. Go think about it for the next 4 years.
     if right_this_minute in odds:
         print("This minute is odd.")
     else:
